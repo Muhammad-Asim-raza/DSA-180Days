dbcf83a06547a62ce2d9f2f5a06794dfa065c25a
Muhammad-Asim-raza <asimraza9659@gmail.com>
2026-01-20 23:55:33 -0800

    TypeCasting Overview

---
+++ Day_01_IfElse/02_Type_Casting.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+        //    TypeCasting is a method of converting a variable
+        //    of one datatype into another.
+int main(){
+           int a= 78;
+           char d;
+           d=a;
+           cout<<d;
+
+           int g;
+           char h='U';
+           g=h;
+           cout<<g;
+}
