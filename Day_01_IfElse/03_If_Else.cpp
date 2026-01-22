6981dcf7a71c4dcf2a54f6488358d0fcdfc47455
Muhammad-Asim-raza <asimraza9659@gmail.com>
2026-01-21 23:05:14 -0800

    Practice If Else Statements.

---
+++ Day_01_IfElse/03_If_Else.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+int main(){
+    //    1. Basic Example 
+    int package;
+    cin>>package;
+    if(package<40){
+        cout<<"Rejected";
+    }else{
+        cout<<"Accepted";
+    }
+
+    // 2. Find Even Or Odd Number. 
+    // int num;
+    // cout<<"Enter Your Number. ";
+    // cin>>num;
+    // if(num%2==0){
+    //     cout<<"Your Number Is An Even Number. ";
+    // }else{
+    //     cout<<"Your Number Is An Odd Number. ";
+    // }
+
+    // 3.Find Positive And Negative Numbers.
+    // int number;
+    // cout<<"Enter Your Number. ";
+    // cin>>number;
+    // if(number>0){
+    //     cout<<"Your Number is Positve. ";
+    // }else if(number==0){
+    //     cout<<"Your Number is Zero. ";
+    // }else{
+    //     cout<<"Your Number is Negative. ";
+    // }
+
+    // 4. Find If Character is Vowel or Consonent. 
+    // int character;
+    // cout<<"Enter Your Character. ";
+    // cin>>character;
+    // if(character=='a'){
+    //     cout<<"Vowel";
+    // }else if(character=='e'){
+    //     cout<<"Vowel";
+    // }
+    // else if(character=='i'){
+    //     cout<<"Vowel";
+    // }
+    // else if(character=='o'){
+    //     cout<<"Vowel";
+    // }
+    // else if(character=='u'){
+    //     cout<<"Vowel";
+    // }else{
+    //     cout<<"Consonent";
+    // }
+}
